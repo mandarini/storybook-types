36b6d26f5368b647ff3c752db23fd8acfecc46eb
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-02-03 18:06:19 +0200

    testing it with pure storybook

--- libs/ui/src/lib/Button/Button.stories.tsx
+++ libs/ui/src/lib/Button/Button.stories.tsx
@@ -12,4 +12,8 @@ export const Basic = Template.bind({});
 Basic.args = {
   disabled: false,
   children: 'Hello',
+  // primary: false,
+  // // eslint-disable-next-line @typescript-eslint/no-empty-function
+  // onClick: () => {},
+  // className: '',
 };
